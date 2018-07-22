9a5dda8c19fc32040cc8776420205a2376735f20
Spencer Christiansen <jc.spencer92@gmail.com>
2018-07-21 22:48:11 -0600

    Get webpack running locally

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  // webpack folder's entry js - excluded from jekll's build process.
+  entry: "./webpack/entry.js",
+  output: {
+    // we're going to put the generated file in the assets folder so jekyll
+    // will grab it.
+      path: path.resolve(__dirname, "src", "assets", "javascripts"),
+      filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'env'],
+        },
+      },
+    ],
+  },
+};
